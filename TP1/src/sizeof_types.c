69d22c8550f66389ce134445ed9fc3a3344e029f
MatPct <piecourtmathieu@gmail.com>
2025-10-02 11:23:58 +0200

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -7,7 +7,30 @@ int main() {
   printf("char : %ld octet(s)\n", sizeof(char));
   printf("signed char : %ld octet(s)\n", sizeof(signed char));
   printf("unsigned char : %ld octet(s)\n", sizeof(unsigned char));
+
+  printf("short : %ld octet(s)\n", sizeof(short));
+  printf("signed short : %ld octet(s)\n", sizeof(signed short));
+  printf("unsigned short : %ld octet(s)\n", sizeof(unsigned short));
+
+  printf("int : %ld octet(s)\n", sizeof(int));
+  printf("signed int : %ld octet(s)\n", sizeof(signed int));
+  printf("unsigned int : %ld octet(s)\n", sizeof(unsigned int));
+
+  printf("long int : %ld octet(s)\n", sizeof(long int));
+  printf("signed long int : %ld octet(s)\n", sizeof(signed long int));
+  printf("unsigned long int : %ld octet(s)\n", sizeof(unsigned long int));
+
+  printf("long long int : %ld octet(s)\n", sizeof(long long int));
+  printf("signed long long int : %ld octet(s)\n", sizeof(signed long long int));
+  printf("unsigned long long int : %ld octet(s)\n", sizeof(unsigned long long int));
+
+  printf("float : %ld octet(s)\n", sizeof(float));
+
+  printf("double : %ld octet(s)\n", sizeof(double));
+
+  printf("long double : %ld octet(s)\n", sizeof(long double));
 }
+
 
 
 
