4863ac14125f5e83d3f52f3d56298b0e6a328bec
MatPct <piecourtmathieu@gmail.com>
2025-10-02 12:12:21 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -6,8 +6,8 @@ int main() {
   int b = 3;
 
   // On affiche les valeurs des deux variables pour plus de clarté sur les résultats ci-dessous.
-  printf("Valeur de la variable a %d", a);
-  printf("Valeur de la variable b %d", b);
+  printf("Valeur de la variable a : %d\n", a);
+  printf("Valeur de la variable b : %d\n", b);
   
   // On affiche le résultat des tests des différents opérateurs arithmétiques.
   printf("L'addition de a et b est égale à : %d\n", a + b);
@@ -17,6 +17,7 @@ int main() {
   printf("Le reste de la division de a par b est égal à : %d\n", a % b);
   printf("Utilisation d'un opérateur de comparaison pour vérifier si a et b sont égaux : %d\n", a == b);
 }
+
 
 
 
