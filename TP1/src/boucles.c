c657d69898f642f1af04134cd165ddb3ee72888c
MatPct <piecourtmathieu@gmail.com>
2025-10-02 12:37:39 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -4,18 +4,28 @@ int main() {
   // Taille du triangle, doit être < 10
   int compteur = 5;
 
+  // On teste la valeur du compteur pour vérifier s'il respecte bien la condition.
   if (compteur >= 10) {
-      printf("La valeur de compteur doit être strictement inférieure à 10.\n");
+      printf("La valeur du compteur doit être strictement inférieure à 10.\n");
   }
   else {
     // Boucle pour les lignes
     for (int i = 1; i <= compteur; i++) {
         // Boucle pour afficher les étoiles sur chaque ligne
         for (int j = 1; j <= i; j++) {
-            printf("*");
+            // Le 1er  et dernier caractère ont le motif : *
+            if (j == 1 || j == i) {
+                printf("*");
+            } 
+            else {
+                // Les caractères internes ont le motif : #
+                printf("#");
+            }
         }
-        printf("\n"); // Retour à la ligne après chaque ligne d'étoiles
+        // Retour à la ligne après chaque ligne d'étoiles
+        printf("\n"); 
     }
   }
 }
+
 
