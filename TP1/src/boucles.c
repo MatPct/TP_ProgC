f8697e14e0e66a474dc9ab8b39e34acd90482389
MatPct <piecourtmathieu@gmail.com>
2025-10-02 12:55:39 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -4,6 +4,7 @@ int main() {
   // Taille du triangle, doit être < 10
   int compteur = 5;
 
+  // Version avec une boucle for.
   // On teste la valeur du compteur pour vérifier s'il respecte bien la condition.
   if (compteur >= 10) {
       printf("La valeur du compteur doit être strictement inférieure à 10.\n");
@@ -26,7 +27,31 @@ int main() {
         printf("\n"); 
     }
   }
+
+  // Version avec une boucle while.
+  if (compteur >= 10) {
+      printf("La valeur du compteur doit être strictement inférieure à 10.\n");
+  }
+  else {
+    int i = 1;
+    while (i <= compteur) {
+        int j = 1; // compteur pour les colonnes
+        while (j <= i) {
+            // Le premier et le dernier de chaque ligne sont toujours '*'
+            if (j == 1 || j == i) {
+                printf("*");
+            } else {
+                printf("#");
+            }
+
+            printf(" "); // espace entre les caractères
+            j++;
+        }
+        printf("\n"); // retour à la ligne après chaque ligne
+        i++;
+  }
 }
+
 
 
 
