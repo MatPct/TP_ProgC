bef67fc1a4526238748cc4da8e8531d6fc84dff6
MatPct <piecourtmathieu@gmail.com>
2025-10-02 12:08:03 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -5,6 +5,12 @@ int main() {
   int a = 16;
   int b = 3;
   
-  printf("L'addition de a et b est égal à : %d\n", a+b);
+  printf("L'addition de a et b est égale à : %d\n", a + b);
+  printf("La soustraction de a et b est égale à : %d\n", a - b);
+  printf("La multiplication de a et b est égale à : %d\n", a * b);
+  printf("La division de a par b est égale à : %d\n", a / b);
+  printf("Le reste de la division de a par b est égal à : %d\n", a % b);
+  printf("Utilisation d'un opérateur de comparaison pour vérifier si a et b sont égaux : %d\n", a == b);
 }
+
 
