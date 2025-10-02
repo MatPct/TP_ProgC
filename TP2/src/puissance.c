2b85c6a4189418bc0f4b9edbfd9663922d5353fc
MatPct <piecourtmathieu@gmail.com>
2025-10-02 15:20:00 +0200

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // base
+    int a = 2;
+    // exposant
+    int b = 3;
+    // stocke le résultat
+    int resultat = 1;
+    int i;
+
+    // Boucle pour calculer a^b
+    for (i = 1; i <= b; i++) {
+        resultat *= a;
+    }
+
+    // Affichage du résultat
+    printf("%d élevé à la puissance %d = %d\n", a, b, resultat);
+}
