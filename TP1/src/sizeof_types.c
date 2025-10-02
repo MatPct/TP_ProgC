66cd38b67577dfac8f46e90baf096ff746c5c02f
MatPct <piecourtmathieu@gmail.com>
2025-10-02 11:36:29 +0200

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -7,28 +7,29 @@ int main() {
   printf("signed char : %zu octet(s)\n", sizeof(signed char));
   printf("unsigned char : %zu octet(s)\n", sizeof(unsigned char));
 
-  printf("short : %ld octet(s)\n", sizeof(short));
-  printf("signed short : %ld octet(s)\n", sizeof(signed short));
-  printf("unsigned short : %ld octet(s)\n", sizeof(unsigned short));
+  printf("short : %zu octet(s)\n", sizeof(short));
+  printf("signed short : %zu octet(s)\n", sizeof(signed short));
+  printf("unsigned short : %zu octet(s)\n", sizeof(unsigned short));
 
-  printf("int : %ld octet(s)\n", sizeof(int));
-  printf("signed int : %ld octet(s)\n", sizeof(signed int));
-  printf("unsigned int : %ld octet(s)\n", sizeof(unsigned int));
+  printf("int : %zu octet(s)\n", sizeof(int));
+  printf("signed int : %zu octet(s)\n", sizeof(signed int));
+  printf("unsigned int : %zu octet(s)\n", sizeof(unsigned int));
 
-  printf("long int : %ld octet(s)\n", sizeof(long int));
-  printf("signed long int : %ld octet(s)\n", sizeof(signed long int));
-  printf("unsigned long int : %ld octet(s)\n", sizeof(unsigned long int));
+  printf("long int : %zu octet(s)\n", sizeof(long int));
+  printf("signed long int : %zu octet(s)\n", sizeof(signed long int));
+  printf("unsigned long int : %zu octet(s)\n", sizeof(unsigned long int));
 
-  printf("long long int : %ld octet(s)\n", sizeof(long long int));
-  printf("signed long long int : %ld octet(s)\n", sizeof(signed long long int));
-  printf("unsigned long long int : %ld octet(s)\n", sizeof(unsigned long long int));
+  printf("long long int : %zu octet(s)\n", sizeof(long long int));
+  printf("signed long long int : %zu octet(s)\n", sizeof(signed long long int));
+  printf("unsigned long long int : %zu octet(s)\n", sizeof(unsigned long long int));
 
-  printf("float : %ld octet(s)\n", sizeof(float));
+  printf("float : %zu octet(s)\n", sizeof(float));
 
-  printf("double : %ld octet(s)\n", sizeof(double));
+  printf("double : %zu octet(s)\n", sizeof(double));
 
-  printf("long double : %ld octet(s)\n", sizeof(long double));
+  printf("long double : %zu octet(s)\n", sizeof(long double));
 }
+
 
 
 
