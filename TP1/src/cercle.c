519f7cdfb15bb79fec4479a0ba39b2ba4ec3d219
MatPct <piecourtmathieu@gmail.com>
2025-10-02 10:27:12 +0200

    Update cercle.c

---
+++ TP1/src/cercle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+  double pi = 3.1416;
+  double r = 6;
+  
+  printf("L'aire du cercle = %f", pi*(r*r));
+  printf("Le périmètre du cercle = %f", 2*pi*r;
+}
