9a1e23986adb6c257cca75f3f1c5b84768331bf1
MatPct <piecourtmathieu@gmail.com>
2025-10-02 11:18:38 +0200

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -4,10 +4,11 @@
 int main() {
   printf("Taille des types de base en C (en octets) :");
   
-  printf("char : %s octet(s)\n", sizeof(char));
+  printf("char : %ld octet(s)\n", sizeof(char));
   printf("signed char : %ld octet(s)\n", sizeof(signed char));
   printf("unsigned char : %ld octet(s)\n", sizeof(unsigned char));
 }
+
 
 
 
